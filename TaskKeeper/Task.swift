a71290fd2d9884aa35d90812d2651d88a74d2b52
Dakota Brown <brown.dakotachase@gmail.com>
2016-12-11 16:35:12 -0500

    Skeleton for App has been completed. Currently there are two view controllers. The main controller has a nav bar and a table view. The nav bar will allow you add a task by performing a segue to a new view controller. Currently this second view controller is empty but it will eventually be the place where you create a new task.

---
+++ TaskKeeper/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  TaskKeeper
+//
+//  Created by Dakota Brown on 12/11/16.
+//  Copyright © 2016 Dakota Brown. All rights reserved.
+//
+
+import Foundation
+
+class Task{
+    var name = ""
+    var important = false
+}
